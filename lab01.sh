f228d1ef1036ff28d7cb2e143add9d7d8d996004
Alessandra de Melo e Silva <ale.melo95@gmail.com>
2018-09-24 18:59:48 -0300

    Add files via upload

---
+++ lab01.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Definindo variaveis
+int_dialog="dialog --stdout --title \"Escolha de interface\" --menu \"Escolha a interface para ser configurada\" 0 0 0"
+int_desc="Interface"
+
+# Iniciando o programa
+dialog --title "Configuração de rede" --msgbox "Bem vindo ao guia de configuração de interfaces de rede" 0 0
+
+# Tela para escolha da VM para config
+VM=$(dialog --stdout --title "Escolha de VM" --menu "Escolha a VM para ser configurada" 0 0 0 VM-A  'Maquina Virtual A' ROT-A "Roteador A")
+
+# Define o array de interfaces de rede disponiveis e apresenta tela para selecionar a desejada
+mapfile -t interface_array < <(basename -a /sys/class/net/*)
+for index in ${!interface_array[@]}; do
+  int_dialog="$int_dialog ${interface_array[index]} $int_desc"
+done
+INT=$(eval $int_dialog)
+
+# Tela para receber IP e mascara da VM
+CIDR=$(dialog --stdout --inputbox 'Digite o número IP e Mascara (modelo CIDR): ' 0 0 )
+
+
+if [ $VM == "VM-A"]
+  then
+    clear >$(tty)
+    echo "Iniciando config da $VM..."
+    ifconfig $INT $CIDR
+elif [ $VM == "ROT-A"]
+  then
+    GW=$(dialog --stdout --inputbox 'Digite o número IP do Gatway padrao: ' 0 0 )
+    clear >$(tty)
+    echo "Iniciando config da $VM..."
+    ifconfig $INT $CIDR
+    echo 1 > /proc/sys/net/ipv4/ip_forward
+    route add default gw $GW
+    iptables -t nat -A POSTROUTING -o $INT -j SNAT --to $GW
+fi
+
+ifconfig -a
+route -n
