f24e3e1d1c50fd8f390eae3286b9a0c397c9c36b
Alessandra de Melo e Silva <ale.melo95@gmail.com>
2018-09-26 16:25:50 -0300

    Update lab01-sudo.sh

--- lab01-sudo.sh
+++ lab01-sudo.sh
@@ -1,45 +1,78 @@
 #!/bin/bash
 #Verificacao de super user
+
 if [[ $EUID -ne 0 ]]; then
   dialog	\
 	--title "Nao e super usuario/root" \
 	--msgbox "Execute o script como super usuario/root" \
 	7 40
-  exit 1
+  [ $? -eq 0 ] && clear >$(tty) && exit 1
 fi
 
 # Definindo variaveis
-int_dialog="dialog --stdout --title \"Escolha de interface\" --menu \"Escolha a interface para ser configurada\" 0 0 0"
-int_desc="Interface"
 
-# Iniciando o programa
-dialog --title "Configuração de rede" --msgbox "Bem vindo ao guia de configuração de interfaces de rede." 7 40
+proxima=primeira
+while : ; do
 
-# Tela para escolha da VM para config
-VM=$(dialog --stdout --title "Escolha de VM" --menu "Escolha a VM para ser configurada" 0 0 0 VM-A  'Maquina Virtual A' ROT-A "Roteador A")
+  case "$proxima" in
+    primeira)
+      proxima=escolhavm
+      # Iniciando o programa
+      dialog --title "Configuração de rede" --msgbox "Bem vindo ao guia de configuração de interfaces de rede." 7 40
+      ;;
 
-# Define o array de interfaces de rede disponiveis e apresenta tela para selecionar a desejada
-mapfile -t interface_array < <(basename -a /sys/class/net/*)
-for index in ${!interface_array[@]}; do
-  int_dialog="$int_dialog ${interface_array[index]} $int_desc"
+    escolhavm)
+      anterior=primeira
+      proxima=escolhaint
+      # Tela para escolha da VM para config
+      VM=$(dialog --stdout --title "Escolha de VM" --menu "Escolha a VM para ser configurada" 0 0 0 VM-A  'Maquina Virtual A' ROT-A "Roteador A")
+      ;;
+
+    escolhaint)
+      anterior=escolhavm
+      proxima=informeip
+      int_dialog="dialog --stdout --title \"Escolha de interface\" --menu \"Escolha a interface para ser configurada\" 0 0 0"
+      int_desc="Interface"
+      # Define o array de interfaces de rede disponiveis e apresenta tela para selecionar a desejada
+      mapfile -t interface_array < <(basename -a /sys/class/net/*)
+      for index in ${!interface_array[@]}; do
+        int_dialog="$int_dialog ${interface_array[index]} $int_desc"
+      done
+      INT=$(eval $int_dialog)
+      ;;
+
+    informeip)
+      anterior=escolhaint
+      # Tela para receber IP e mascara da VM
+      CIDR=$(dialog --stdout --inputbox 'Digite o número IP e Mascara (modelo CIDR).\nEx:192.168.0.1/24 ' 0 0 )
+      [ $? -eq 0 ] && break
+      ;;
+
+    *)
+      echo "Janela inexistente '$proxima'."
+      echo "Fechando o programa..."
+      exit
+  esac
+
+retorno=$?
+[ $retorno -eq 1 ] && proxima=$anterior  #cancelar
+[ $retorno -eq 255 ] && clear >$(tty) && exit 1  #esc
 done
-INT=$(eval $int_dialog)
-
-# Tela para receber IP e mascara da VM
-CIDR=$(dialog --stdout --inputbox 'Digite o número IP e Mascara (modelo CIDR).\nEx:192.168.0.1/24 ' 0 0 )
 
 
 if [ "$VM" == "VM-A" ];
   then
+    # VM da rede interna necessita de IP, GW (IP rede interna ROT-A)
+    GW=$(dialog --stdout --inputbox 'Digite o número IP do Gatway padrao: ' 0 0 )
     clear >$(tty)
+    [ $? -eq 1 ] && exit
     echo "Iniciando config da $VM..."
     ifconfig $INT $CIDR
-    GW=$(dialog --stdout --inputbox 'Digite o número IP do Gatway padrao: ' 0 0 )
     route add default gw $GW
 elif [ "$VM" == "ROT-A" ];
   then
-    #Tipo de rede. Se for interna, so precisa de IP. Se for NAT, precisa de GW e fazer Ip_forward
-    #E NAT no iptables
+    #Tipo de rede. Se for interna, so precisa de IP
+    #Se for NAT, precisa de GW e fazer Ip_forward NAT no iptables
     TIPOREDE=$(dialog --stdout --title "Escolha o tipo da rede"  --menu "Escolha o tipo da rede desta interface" \
                0 0 0\
                Interna 'Rede interna'\
